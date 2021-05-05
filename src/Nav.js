22d8bfb87422bff1767eb1afecfd292014996ae3
sunilrajthota <sunilrajt.128@gmail.com>
2021-05-05 14:55:10 -0400

    Initial Commit

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://pbs.twimg.profile_images/124011999041155"
+        alt="Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
